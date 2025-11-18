e5bc174346a2b227ae2af69a37c3cc030ff03828
Krisna Gupta <imade.krisna@poltekapp.ac.id>
2025-11-18 08:45:30 +0700

    weqs

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# hgbt_app
-Repo paper bersama teman-teman APP
+# Tentang repo ini
+Ini adalah repository untuk paper yang dikerjakan bersama dengan Yustika Pambudi, Dhisa Priyadi dan Dinar Safa. Kami meneliti tentang dampak Harga Gas Bumi Tertentu (HGBT) terhadap peningkatan nilai tambah dari industri manufaktur di Indonesia.
+
+Repo ini hanya berfokus pada kode untuk mengeluarkan _summary statistics_ dan hasil regresi dari paper tersebut. Semua proses pengumpulan data dilakukan oleh para peneliti dan tidak termasuk dalam repo ini. Datanya sendiri dapat dilihat di folder `dat`.
+
+Di repo ini ada `index.qmd` yang berisi kode untuk menghasilkan dokumennya dalam bentuk html dan docx. Disiapkan juga `hgbt.r` yang berisikan file R replikasi untuk menghasilkan tabel-tabel yang ada di dokumen. untuk run `hgbt.r` hanya diperlukan folder `dat` beserta isinya dan folder `tab` untuk lokasi penyimpanan tabel regresi dalam bentuk excel.
+
+Jika repo ini bermanfaat untuk anda, mohon dapat sitasi tulisan ini dengan:
+
+> Gupta, K., Pambudi, Y., Priyadi, D., & Safa, D. (2025). Bagaimana harga gas bumi tertentu mempengaruhi industri nasional? Unpublished.
+
+atau jika anda menggunakan bibtex:
+
+```{bibtex}
+@article{hgbt,
+author = {Gupta, Krisna and Pambudi, Yustika and Priyadi, Dhisa and Safa, Dinar},
+title = {Bagaimana harga gas bumi tertentu mempengaruhi industri nasional?},
+year = {2025},
+journal = {Unpublished}
+}
+```
--- hgbt.r
+++ hgbt.r
@@ -3,7 +3,7 @@ library(readxl)         # wajib buat baca data excel
 library(fixest)         # Buat regresi data panel
 library(modelsummary)   # buat bikin tabel deskripsi statistik dan hasil regresi
 
-# Data prep
+# Data prep and summary
 dat<-read_excel("dat/HGBT.xlsx",sheet="data") # baca data kalian dari excel
 dut<-read_excel("dat/wpi.xlsx")               # baca data indeks harga produksi, ambil dari BPS
 dat<-dat|>inner_join(dut) # gabungin wpi ke HGBT
@@ -20,3 +20,34 @@ dat$lV<-log(dat$V)
 datasummary(L+M+K+R+V~factor(HGBT)*(Mean+Median+SD),data=dat,
   notes=c('sumber: penulis'), output="tab/sumstat.xlsx")
 
+# Regresi
+## Non deflated
+
+reg1<-feols(data=dat,lV~lK+lL+HGBT)
+reg2<-feols(data=dat,lV~lK+lL+HGBT|Perusahaan) # tanda | buat fixed effect
+reg3<-feols(data=dat,lV~lK+lL+HGBT|Perusahaan+Tahun)
+
+regtab<- list( # bikin dict buat modelsummary
+  "OLS" = reg1,
+  "FE"  = reg2,
+  "TWFE" = reg3
+)
+modelsummary(regtab,stars=TRUE,gof_omit = 'FE|IC|RMSE|Std.|Adj.',
+  notes=c('sumber: olahan penulis'),output="tab/regression_nominal.xlsx")
+
+## Deflated
+
+regtab1<- list(
+  "OLS" = reg4,
+  "FE"  = reg5,
+  "TWFE" = reg6
+)
+
+reg4<-feols(data=dat,lV1~lK1+lL1+HGBT)
+reg5<-feols(data=dat,lV1~lK1+lL1+HGBT|Perusahaan)
+reg6<-feols(data=dat,lV1~lK1+lL1+HGBT|Perusahaan+Tahun)
+
+modelsummary(regtab1,stars=TRUE,gof_omit = 'FE|IC|RMSE|Std.|Adj.',
+  notes=c('sumber: olahan penulis'),output="tab/regression_deflated.xlsx")
+
+# end of file
